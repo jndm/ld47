a921a82ac84dae1cd17f5579885175768186e50d
Joonas <jondemm@gmail.com>
2020-10-04 17:34:17 +0300

    added wall textures, rotate start and end planets

---
+++ Assets/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    protected Vector3 rotationDirection;
+
+    [SerializeField]
+    protected float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(rotationDirection, this.rotationSpeed * Time.deltaTime);
+    }
+}
