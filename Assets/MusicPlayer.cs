eaf290223b466f65c11d1e518bf546f7bd3a2c23
Nikke Vartia <nikke.vartia@kuuasema.com>
2020-10-04 23:11:31 +0300

    Bug fixes

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -6,12 +6,16 @@ public class MusicPlayer : MonoBehaviour
 {
 
     private AudioSource music;
+    private static MusicPlayer instance;
     // Start is called before the first frame update
     void Start()
     {
-        music = GetComponent<AudioSource>();
-        music.Play();
-        music.volume = 0.5f;
-        DontDestroyOnLoad(gameObject);
+        if (instance == null) {
+            instance = this;
+            music = GetComponent<AudioSource>();
+            music.Play();
+            music.volume = 0.5f;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
